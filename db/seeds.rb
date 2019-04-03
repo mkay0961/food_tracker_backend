319e68a758371dca036a3ae548727febd8b55466
mkay0961 <mkay0961@gmail.com>
2019-04-02 21:22:35 -0400

    adding seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+require 'csv'
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,6 +6,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+csv_text = File.read('/Users/MatthewKay/Development/Flatiron/Mod5/Food_Tracker/food_tracker_backend/db/Test.csv')
+csv = CSV.parse(csv_text, :headers => true)
+# puts csv[2]["Name"]
+puts csv.count
+# puts csv[2]["Categories"]
+# puts csv[2]["Price"].slice(2,csv[2]["Price"].length)
+for x in (2...csv.count) do
+  Food.find_or_create_by(name: csv[x]["Name"],
+                         category: csv[x]["Categories"] ,
+                         default_expiration: "07/25/2020" )
+end
+
 puts "Destroying all"
 
 User.destroy_all
