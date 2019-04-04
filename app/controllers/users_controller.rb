40a95dc6dc95cb086d8682fd83c0102a2fec5b5d
mkay0961 <mkay0961@gmail.com>
2019-04-03 21:31:37 -0400

    working on updating backend

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,4 +5,24 @@ class UsersController < ApplicationController
     render json: @user
   end
 
+  # def update
+  #   @user = User.find(params[:id])
+  #   if(!params["food"].nil?)
+  #     params["food"].each do |food|
+  #       UserFood.create(user_id: @user.id,
+  #                               food_id: food["id"],
+  #                               active: true,
+  #                               amount: "",
+  #                               price: 5.00,
+  #                               expiration_date: "04/4/2019",
+  #                               expired: false)
+  #     end
+  #   end
+  #
+  #   # if(!params["userInfo"].nil?)
+  #   #
+  #   # end
+  #   byebug
+  # end
+
 end
