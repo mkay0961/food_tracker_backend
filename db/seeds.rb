de4761b0498efc9c164cdfe3302113bde493c608
mkay0961 <mkay0961@gmail.com>
2019-04-18 12:59:21 -0400

    Seed fixed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@ Recipe.destroy_all
 RecipeFood.destroy_all
 puts "Destroyed all"
 
-csv_text = File.read('/Users/MatthewKay/Development/Flatiron/Mod5/Food_Tracker/food_tracker_backend/db/Test.csv')
+csv_text = File.read('/Users/MatthewKay/Development/Flatiron/Mod5/Food_Tracker/food_tracker_backend/db/Test3.csv')
 csv = CSV.parse(csv_text, :headers => true)
 
 
@@ -15,12 +15,23 @@ csv = CSV.parse(csv_text, :headers => true)
 
 puts "Done destroying, starting to seed"
 
-for x in (2...csv.count) do
-  Food.find_or_create_by(name: csv[x]["Name"],
-                         image: csv[x]["Image"].split("(")[1].split(")")[0],
-                         category: csv[x]["Categories"] ,
-                         default_expiration: Faker::Date.forward(130),
-                         unit: "oz")
+for x in (0..csv.count) do
+  if(csv[x].nil?)
+    puts "y"
+  else
+    puts "FOOOOOOD"
+    puts csv[x]["Name"]
+    puts csv[x]["Image"].split("(")[1].split(")")[0]
+    puts csv[x]["Categories"]
+    puts csv[x]["Unit"]
+    puts "FOOOOOOD"
+    Food.find_or_create_by(name: csv[x]["Name"],
+                           image: csv[x]["Image"].split("(")[1].split(")")[0],
+                           category: csv[x]["Categories"] ,
+                           default_expiration: Faker::Date.forward(130),
+                           unit: csv[x]["Unit"])
+  end
+
 end
 
 
@@ -33,6 +44,11 @@ matthew = User.create(username: "mkay0961",
 
 80.times do
   f = Food.find(rand(1..Food.all.count))
+  # puts "NON"
+  # puts f
+  # puts "#{rand(1..15)} #{f.unit}"
+  # puts Faker::Date.forward(130)
+  # puts "NON"
   userfood2 = UserFood.create(user_id: matthew.id,
                         food_id: f.id,
                         active: true,
@@ -44,8 +60,15 @@ end
 
 20.times do
   f = Food.find(rand(1..Food.all.count))
-
   d = Faker::Date.backward(90)
+  # puts "EXP"
+  # puts f
+  # puts "#{rand(1..30)}.#{rand(1..99)}"
+  # puts d
+  # puts Faker::Date.between(d, Date.today)
+  # puts "EXP"
+
+  #
   userfood2 = UserFood.create(user_id: matthew.id,
                         food_id: f.id,
                         active: true,
@@ -53,15 +76,20 @@ end
                         price: "#{rand(1..30)}.#{rand(1..99)}",
                         expiration_date: Faker::Date.between(d, Date.today),
                         created_at: d,
-                        expired: false)
+                        expired: true)
 end
 
+csv_text2 = File.read('/Users/MatthewKay/Development/Flatiron/Mod5/Food_Tracker/food_tracker_backend/db/recipes.csv')
+csv2 = CSV.parse(csv_text2, :headers => true)
+
+
+cat = ["Dinner", "Lunch", "Breakfast"]
 
 100.times do
   recipe2 = Recipe.create(title: Faker::Food.dish,
                        description: Faker::Food.description,
-                       instructions:"1.Cut chicken 2.Cook chicken 1.Cut chicken 2.Cook chicken 1.Cut chicken 2.Cook chicken 1.Cut chicken 2.Cook chicken ",
-                       category: "Dinner")
+                       instructions:csv2["Directions"][rand(0..csv2.count)],
+                       category: cat[rand(0..cat.size-1)])
  rand(1..6).times do
   f = Food.find(rand(1..Food.all.count))
   rec2food1 = RecipeFood.create(recipe_id: recipe2.id,
